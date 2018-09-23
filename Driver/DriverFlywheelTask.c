53cbf96fc78ff08e932929b70904e5ce0d19b6c3
theol0403 <16546293+theol0403@users.noreply.github.com>
2018-09-23 15:03:49 -0700

    uncomment

--- Driver/DriverFlywheelTask.c
+++ Driver/DriverFlywheelTask.c
@@ -1,68 +1,68 @@
-// PID flywheelPID;
-// bool flywheelToggle = false;
-//
-// //max speed = 750;
-//
-// task flywheelTask()
-// {
-// 	pidInit(flywheelPID, 0.169, 0, 0);
-//
-// 	while(true) 
-// 	{
-//
-// 		if(vexRT[Btn6U])
-// 		{
-// 			flywheelToggle = true
-// 		}
-// 		else if(vexRT[Btn6D])
-// 		{
-// 			flywheelToggle = false;
-// 		}
-//
-//
-// 		if(flywheelToggle)
-// 		{
-// 			setFlywheelPower
-// 		}
-//
-// 		for(int i = 40; i < 127; i++)
-// 		{
-// 			setFlywheelPower(i);
-// 			delay(60);
-// 		}
-// 		delay(1000000);
-// 	}
-// }
-//
-//
-//
-//
-//
-//
-// /*
-//
-//
-//
-//
-// task DriverFlywheelTask()
-// {
-//
-// while(true)
-// {
-//
-// }
-// }
-//
-//
-// // Flywheel BangBang --------------------------------------------------------------------------------
-// if(vexRT[Btn6U])
-// {
-// startTask(flywheelTask);
-// }
-// else if(vexRT[Btn6D])
-// {
-// stopTask(flywheelTask);
-// setFlywheelPower(0);
-// }
-// // Flywheel BangBang --------------------------------------------------------------------------------
-// */
+PID flywheelPID;
+bool flywheelToggle = false;
+
+//max speed = 750;
+
+task flywheelTask()
+{
+	pidInit(flywheelPID, 0.169, 0, 0);
+
+	while(true)
+	{
+
+		if(vexRT[Btn6U])
+		{
+			flywheelToggle = true
+		}
+		else if(vexRT[Btn6D])
+		{
+			flywheelToggle = false;
+		}
+
+
+		if(flywheelToggle)
+		{
+			setFlywheelPower
+		}
+
+		for(int i = 40; i < 127; i++)
+		{
+			setFlywheelPower(i);
+			delay(60);
+		}
+		delay(1000000);
+	}
+}
+
+
+
+
+
+
+/*
+
+
+
+
+task DriverFlywheelTask()
+{
+
+while(true)
+{
+
+}
+}
+
+
+// Flywheel BangBang --------------------------------------------------------------------------------
+if(vexRT[Btn6U])
+{
+startTask(flywheelTask);
+}
+else if(vexRT[Btn6D])
+{
+stopTask(flywheelTask);
+setFlywheelPower(0);
+}
+// Flywheel BangBang --------------------------------------------------------------------------------
+*/
